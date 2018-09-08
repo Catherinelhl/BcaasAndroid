bd7727e88b0e8001205a647034710e780db55fec
Catherine liu <catherineliu@orangeblock.tech>
2018-09-08 20:01:49 +0800

    modify

--- app/src/main/java/io/bcaas/ui/activity/LoginActivity.java
+++ app/src/main/java/io/bcaas/ui/activity/LoginActivity.java
@@ -104,7 +104,6 @@ public class LoginActivity extends BaseActivity
         Disposable subscribeUnlockWallet = RxView.clicks(btnUnlockWallet)
                 .throttleFirst(Constants.ValueMaps.sleepTime800, TimeUnit.MILLISECONDS)
                 .subscribe(o -> {
-                    intentToActivity(MainActivity.class);
                     if (BcaasApplication.existKeystoreInDB()) {
                         String password = etPassword.getText().toString();
                         if (StringTool.notEmpty(password)) {
