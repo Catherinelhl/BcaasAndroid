144213fa1b1af5c0f4969d7522410a840f183433
catherineliu <catherineliu@orangeblock.tech>
2018-09-13 20:21:25 +0800

    「检查钱包信息」成功发送邮件携带附件

--- app/src/main/java/io/bcaas/ui/activity/CheckWalletInfoActivity.java
+++ app/src/main/java/io/bcaas/ui/activity/CheckWalletInfoActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -27,6 +28,7 @@ import com.jakewharton.rxbinding2.view.RxView;
 import com.squareup.otto.Subscribe;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -52,6 +54,8 @@ import io.bcaas.tools.ecc.WalletTool;
 import io.bcaas.ui.contracts.CheckWalletInfoContract;
 import io.bcaas.vo.PublicUnitVO;
 import io.reactivex.disposables.Disposable;
+
+import static android.support.v4.content.FileProvider.getUriForFile;
 
 /**
  * @author catherine.brainwilliam
@@ -105,6 +109,10 @@ public class CheckWalletInfoActivity extends BaseActivity implements CheckWallet
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE};
+
+    //得到需要存储当前keystore信息的文件
+    private File file;
+
 
     @Override
     public int getContentView() {
@@ -205,11 +213,18 @@ public class CheckWalletInfoActivity extends BaseActivity implements CheckWallet
         Disposable subscribeSendEmail = RxView.clicks(btnSendEmail)
                 .throttleFirst(Constants.ValueMaps.sleepTime800, TimeUnit.MILLISECONDS)
                 .subscribe(o -> {
-                    if (BcaasApplication.isRealNet()) {
-                        checkWriteStoragePermission(CheckWalletInfoActivity.this);
+                    getExternalFile();
+                    if (file != null) {
+                        if (BcaasApplication.isRealNet()) {
+                            checkWriteStoragePermission(CheckWalletInfoActivity.this);
+                        } else {
+                            showToast(getResources().getString(R.string.network_not_reachable));
+                        }
                     } else {
-                        showToast(getResources().getString(R.string.network_not_reachable));
+                        // 如果当前文件夹返回的文件信息为空，提示当前不能发送邮件
+                        showToast(getResources().getString(R.string.cannot_send_email));
                     }
+
                 });
         Disposable subscribeCurrency = RxView.clicks(tvCurrency)
                 .throttleFirst(Constants.ValueMaps.sleepTime800, TimeUnit.MILLISECONDS)
@@ -221,6 +236,25 @@ public class CheckWalletInfoActivity extends BaseActivity implements CheckWallet
             return false;
         });
 
+    }
+
+    /*得到一个存储当前keystore的文件地址*/
+    private void getExternalFile() {
+        //得到需要创建的文件夹
+        File rootFile = new File(getExternalFilesDir(Constants.KeyMaps.BCAAS_DIR_NAME).getAbsolutePath());
+        if (!rootFile.exists()) {
+            rootFile.mkdir();
+        }
+        //得到需要创建用来存储信息的文件,文件夹是一个以当前钱包地址命名的txt
+        file = new File(rootFile, FilePathTool.getKeyStoreFileName(BcaasApplication.getWalletAddress()));
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                LogTool.d(TAG, e.getMessage());
+                e.printStackTrace();
+            }
+        }
     }
 
     /*重新选择币种返回监听*/
@@ -283,7 +317,7 @@ public class CheckWalletInfoActivity extends BaseActivity implements CheckWallet
                 // 没有写的权限，去申请写的权限，会弹出对话框
                 ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
             } else {
-                presenter.getWalletFileFromDB();
+                presenter.getWalletFileFromDB(file);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -298,7 +332,7 @@ public class CheckWalletInfoActivity extends BaseActivity implements CheckWallet
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //这里已经获取到了摄像头的权限，想干嘛干嘛了可以
                     LogTool.d(TAG, MessageConstants.HAD_WRITE_PERMISSION);
-                    presenter.getWalletFileFromDB();
+                    presenter.getWalletFileFromDB(file);
                 } else {
                     //这里是拒绝给APP摄像头权限，给个提示什么的说明一下都可以。
                     LogTool.d(TAG, MessageConstants.WRITE_PERMISSION_REFUSED);
@@ -311,18 +345,23 @@ public class CheckWalletInfoActivity extends BaseActivity implements CheckWallet
      * 发送邮件
      */
     private void sendEmail() {
-        Uri uri = FileProvider.getUriForFile(this,
-                getPackageName() + Constants.ValueMaps.FILEPROVIDER,
-                new File(FilePathTool.getKeyStoreFileName(BcaasApplication.getWalletAddress())));
+        //如果当前手机版本7.0以上，需要根据规则利用fileprovider来send
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Uri uri = getUriForFile(this, getPackageName() + Constants.ValueMaps.FILEPROVIDER, file);
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            //当无法确认发送类型的时候使用如下语句
+            intent.setType("*/*");
+            LogTool.d(TAG, uri);
+            intent.putExtra(Intent.EXTRA_STREAM, uri);
+            startActivityForResult(intent, SEND_EMAIL_OK);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            //当无法确认发送类型的时候使用如下语句
+            intent.setType("*/*");
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            startActivityForResult(intent, SEND_EMAIL_OK);
+        }
 
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.email_subject));
-        intent.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.email_body));
-        intent.setType(Constants.ValueMaps.EMAIL_TYPE);
-//        intent.setType(“*/*”);
-        System.out.println(uri);
-        intent.putExtra(Intent.EXTRA_STREAM, uri);
-        startActivityForResult(intent, SEND_EMAIL_OK);
     }
 
 
