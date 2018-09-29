7003080cd68b1ed038708c014673de8ae92c6779
catherineliu <catherineliu@orangeblock.tech>
2018-09-29 12:34:39 +0800

    添加receiveamount

--- app/src/main/java/io/bcaas/http/MasterServices.java
+++ app/src/main/java/io/bcaas/http/MasterServices.java
@@ -220,8 +220,7 @@ public class MasterServices {
             transactionChainReceiveVO.setBlockTxType(Constants.ValueMaps.BLOCK_TX_TYPE);
             transactionChainReceiveVO.setSourceTxhash(sourceTxHash);
             transactionChainReceiveVO.setAmount(DecimalTool.transferStoreDatabase(amount));
-            // TODO: 2018/9/28 暂时注视 ，线上服务器没有数据
-//            transactionChainReceiveVO.setReceiveAmount(receiveAmount);
+            transactionChainReceiveVO.setReceiveAmount(receiveAmount);
             transactionChainReceiveVO.setRepresentative(representative);
             transactionChainReceiveVO.setWallet(BcaasApplication.getWalletAddress());
             transactionChainReceiveVO.setWork(Constants.ValueMaps.DEFAULT_REPRESENTATIVE);
