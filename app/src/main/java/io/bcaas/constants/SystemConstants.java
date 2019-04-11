663d4500c4159530c2608961e069aa399b218cff
pengyiqiao <costapeng@orangeblock.tech>
2019-04-11 11:25:05 +0800

    SIT -> PRD

--- app/src/main/java/io/bcaas/constants/SystemConstants.java
+++ app/src/main/java/io/bcaas/constants/SystemConstants.java
@@ -15,8 +15,8 @@ package io.bcaas.constants;
 public class SystemConstants {
 
     /*存储当前连接服务器的类型 国际PRD*/
-//    public static String serverType = Constants.ServerType.INTERNATIONAL_PRD;
-    public static String serverType = Constants.ServerType.INTERNATIONAL_SIT;
+    public static String serverType = Constants.ServerType.INTERNATIONAL_PRD;
+//    public static String serverType = Constants.ServerType.INTERNATIONAL_SIT;
     /****************HTTP [SFN] API [START] ****************/
 
     /*Internet*/
